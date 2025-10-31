a10ea5175f3289497abb2f13c75cff2d20084f2c
Rejaol <karimsalahuddin03@gmail.com>
2025-10-31 11:10:31 +0600

    fixed  all user card also without task - version 7

--- src/firebase.js
+++ src/firebase.js
@@ -8,7 +8,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyB9iT0JXFOwG38uTjY8dYDa527dujrFjBs",
   authDomain: "my-todo-app-69bbe.firebaseapp.com",
   projectId: "my-todo-app-69bbe",
-  storageBucket: "my-todo-app-69bbe.firebasestorage.app",
+  storageBucket: "my-todo-app-69bbe.appspot.com",
   messagingSenderId: "844616107737",
   appId: "1:844616107737:web:b7f37df2b1aecbd240a0c8"
 };
